059d7e9eb26f51d16f0354e810e8130d4879ffb4
Viktar Rudzenia <viktarrudzenia@gmail.com>
2020-09-19 14:45:56 +0300

    feat: complete third part of gatsby tutorial

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,6 +1,8 @@
 import React from "react"
 import { Link } from "gatsby"
+
 import Header from "../components/header"
+import Layout from "../components/layout"
 
 export default function Home() {
   return (
@@ -9,6 +11,15 @@ export default function Home() {
       <Header headerText="Hello Gatsby!" />
       <p>What a world.</p>
       <img src="https://source.unsplash.com/random/400x200" alt="" />
+      <div style={{ margin: `3rem auto`, maxWidth: 600 }}>
+        <Layout>
+          <h1>Hi! I'm building a fake Gatsby site as part of a tutorial!</h1>
+          <p>
+            What do I like to do? Lots of course but definitely enjoy building
+            websites.
+          </p>
+        </Layout>
+      </div>
     </div>
   )
 }
